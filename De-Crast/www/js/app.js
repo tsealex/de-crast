a15c72e00d57bda503b3888dd04638380ce2c5b2
tdohm <timdohm@gmail.com>
2017-03-10 23:13:20 -0600

    created tabbed template, added new html pages/controllers

--- De-Crast/www/js/app.js
+++ De-Crast/www/js/app.js
@@ -3,45 +3,121 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var decrast = angular.module('decrast', ['ionic'])
-
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('decrast', ['ionic', 'decrast.controllers', 'decrast.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-decrast.config(function ($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider) {
 
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
-    .state('index', {
-      url: '/',
-      abstract: true,
-      templateUrl: 'index.html'
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+    .state('addTask', {
+      url: '/addTask',
+      views: {
+        'addTask': {
+          templateUrl: 'templates/task.html',
+          controller: 'AddTaskCtrl'
+        }
+      }
     })
-    .state('home', {
-      url: '/home',
-      templateUrl: 'templates/home.html',
-      controller: 'homeCon'
+    .state('viewTask', {
+      url: '/viewTask',
+      views: {
+        'viewTask': {
+          templateUrl: 'templates/task.html',
+          controller: 'ViewTaskCtrl'
+        }
+      }
+    })
+    .state('editTask', {
+      url: '/editTask',
+      views: {
+        'editTask': {
+          templateUrl: 'templates/task.html',
+          controller: 'EditTaskCtrl'
+        }
+      }
+    })
+
+  .state('tab.ftasks', {
+      url: '/ftasks',
+      views: {
+        'tab-ftasks': {
+          templateUrl: 'templates/tab-ftasks.html',
+          controller: 'FtasksCtrl'
+        }
+      }
+    })
+
+  .state('tab.friends', {
+    url: '/friends',
+    views: {
+      'tab-friends': {
+        templateUrl: 'templates/tab-friends.html',
+        controller: 'FriendsCtrl'
+      }
+    }
+  })
+
+    .state('tab.notif', {
+      url: '/notif',
+      views: {
+        'tab-notif': {
+          templateUrl: 'templates/tab-notifications.html',
+          controller: 'NotifCtrl'
+        }
+      }
+    })
+
+    .state('tab.settings', {
+      url: '/settings',
+      views: {
+        'settings': {
+          templateUrl: 'templates/settings.html',
+          controller: 'SettingsCtrl'
+        }
+      }
     });
 
-  $urlRouterProvider.otherwise('/home');
-});
-
-
-decrast.controller('homeCon', function($scope, $ionicModal, $ionicLoading, $state) {
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
 
 });
---
+++ De-Crast/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('decrast.controllers', [])
+
+.controller('HomeCtrl', function($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state ,$stateParams) {
+
+})
+  .controller('AddTaskCtrl', function($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state ,$stateParams) {
+
+  })
+
+  .controller('EditTaskCtrl', function($scope) {})
+
+  .controller('ViewTaskCtrl', function($scope) {})
+
+.controller('FtasksCtrl', function($scope) {
+})
+  .controller('NotifCtrl', function($scope, $stateParams) {
+  })
+.controller('FriendsCtrl', function($scope) {
+})
+
+  .controller('SettingsCtrl', function($scope) {
+  });
