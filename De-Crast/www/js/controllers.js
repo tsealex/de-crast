93cd5123e9493c551adf1f48ae532f63ecd71f96
Emily Tran <emilytran@etran-mcp.local>
2017-03-11 22:10:15 -0600

    Added popover for Settings button

--- De-Crast/www/js/controllers.js
+++ De-Crast/www/js/controllers.js
@@ -1,40 +1,77 @@
 //Home of controllers. Most of our logic will go here.
 angular.module('decrast.controllers', [])
 
-.controller('HomeCtrl', function($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
-    $scope.name = "De-Crast User";
-    $scope.onClick = function() {
-      $state.go('addTask', {});
-    };
-    $scope.goSettings = function() {
-        $state.go('settings', {});
-    };
-})
-  .controller('AddTaskCtrl', function($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
-      $scope.pagename = "Add";
+    .controller('HomeCtrl', function ($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
+        $scope.name = "De-Crast User";
+        $scope.onClick = function () {
+            $state.go('addTask', {});
+        };
+        $scope.goSettings = function () {
+            $state.go('settings', {});
+        };
+    })
+    .controller('AddTaskCtrl', function ($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
+        $scope.pagename = "Add";
 
-  })
+    })
 
-  .controller('EditTaskCtrl', function($scope) {
-      $scope.pagename = "View";
-  })
+    .controller('EditTaskCtrl', function ($scope) {
+        $scope.pagename = "View";
+    })
 
-  .controller('ViewTaskCtrl', function($scope) {})
+    .controller('ViewTaskCtrl', function ($scope) {
+    })
 
-.controller('FtasksCtrl', function($scope, Ftasks) {
-    $scope.ftasks = Ftasks.all();
-})
-  .controller('NotifCtrl', function($scope, $stateParams, Notifications) {
-      $scope.notifications = Notifications.all();
-  })
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-  $scope.turnStar = function(index){
-    //console.log("You turn star");
-    document.getElementById("starRate"+index).className = 
-      (document.getElementById("starRate"+index).className == "icon ion-ios-star") ? "icon ion-ios-star-outline" : "icon ion-ios-star";
-  }
-})
+    .controller('FtasksCtrl', function ($scope, Ftasks) {
+        $scope.ftasks = Ftasks.all();
+    })
+    .controller('NotifCtrl', function ($scope, $stateParams, Notifications) {
+        $scope.notifications = Notifications.all();
+    })
+    .controller('FriendsCtrl', function ($scope, Friends) {
+        $scope.friends = Friends.all();
+        $scope.turnStar = function (index) {
+            //console.log("You turn star");
+            document.getElementById("starRate" + index).className =
+                (document.getElementById("starRate" + index).className == "icon ion-ios-star") ? "icon ion-ios-star-outline" : "icon ion-ios-star";
+        }
+    })
 
-  .controller('SettingsCtrl', function($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
-  });
+    .controller('SettingsCtrl', function ($rootScope, $scope, $ionicModal, $ionicLoading, $ionicViewSwitcher, $state) {
+    })
+
+    .controller('MainCtrl', function($scope, $ionicPopover) {
+
+        // // .fromTemplate() method
+        // var template = '<ion-popover-view><ion-header-bar><h1 class="title">My Popover Title</h1></ion-header-bar><ion-contentHello!</ion-content></ion-popover-view>';
+        //
+        // $scope.popover = $ionicPopover.fromTemplate(template, {
+        //     scope: $scope
+        // });
+
+        // .fromTemplateUrl() method
+        $ionicPopover.fromTemplateUrl('templates/settings.html', {
+            scope: $scope
+        }).then(function(popover) {
+            $scope.popover = popover;
+        });
+
+        $scope.openPopover = function($event) {
+            $scope.popover.show($event);
+        };
+        $scope.closePopover = function() {
+            $scope.popover.hide();
+        };
+        //Cleanup the popover when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.popover.remove();
+        });
+        // Execute action on hidden popover
+        $scope.$on('popover.hidden', function() {
+            // Execute action
+        });
+        // Execute action on remove popover
+        $scope.$on('popover.removed', function() {
+            // Execute action
+        });
+    });
