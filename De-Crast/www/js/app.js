ef351440c25aff163bd3a6a1e441cc74c688f9b1
tdohm <timdohm@gmail.com>
2017-03-13 22:29:27 -0500

    began additions of cordova plugin for local notifications

--- De-Crast/www/js/app.js
+++ De-Crast/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('decrast', ['ionic', 'decrast.controllers', 'decrast.services', 'decrast.server'])
+angular.module('decrast', ['ionic', 'decrast.controllers', 'decrast.services', 'decrast.server', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
