27274389ea0539bdd6c0930f04b568115fd5cd3f
tsealex <atse@wisc.edu>
2017-03-16 18:49:45 -0500

    changed the code to match what's specified in the design document

--- decrast_server/rest/errors.py
+++ decrast_server/rest/errors.py
@@ -1,15 +1,17 @@
 from django.http import JsonResponse
 
+import sys
+
 class APIError(Exception):
-	def __init__(self, value=0, details=None):
+	def __init__(self, value=0, detail=None):
 		self.code = value
 		self.msg = errors.get(value)
-		self.details = details
+		self.detail = detail
 		if self.msg is None:
 			self.msg = 'unknown error'
 
 	def __str__(self):
-		return repr('Error {}: {} ({})'.format(self.code, self.msg, self.details))
+		return repr('Error {}: {} ({})'.format(self.code, self.msg, self.detail))
 
 class ExceptionMiddleware(object):
 	def __init__(self, get_response):
@@ -20,21 +22,24 @@ class ExceptionMiddleware(object):
 
 	def process_exception(self, request, exception):
 		if not isinstance(exception, APIError):
+			e_type, e_obj, e_tb = sys.exc_info()
+			print('ERROR at line {} in {}: {}'.format(e_tb.tb_frame.f_code \
+				.co_filename, e_tb.tb_lineno, e_type))
 			return JsonResponse({
-				'err_code': 80,
-				'err_msg': 'internal error',
+				'errorCode': 80,
+				'errorMsg': 'internal error',
 				'details': exception or '',
 			})
 		return JsonResponse({
 			'err_code': exception.code,
 			'err_msg': exception.msg,
-			'details': exception.details or '',
+			'detail': exception.detail or '',
 		})
 
 errors = {
 	80: 'internal error',
 	# err_code: err_msg
-	100: 'malformatted request body',
+	100: 'malformed request body',
 	105: 'user does not exist ',
 	110: 'facebook authentication failed',
 	115: 'invalid access token',
@@ -43,11 +48,14 @@ errors = {
 	130: 'required facebook permission not granted',
 	135: 'disabled user account', # not in use
 	140: 'no access token provided',
-	145: 'malformatted request header',
+	145: 'malformed request header',
 	150: 'userId and refresh token do not match',
 	155: 'invalid refresh token',
 	160: 'userId and access token do not match',
 	165: 'invalid input',
 	170: 'unknown error',
 	175: 'illegal URL parameters',
+	180: 'does not exist',
+	190: 'already exists',
+	195: 'bad url query'
 }
--- decrast_server/rest/serializers.py
+++ decrast_server/rest/serializers.py
@@ -3,22 +3,41 @@ from .models import User
 from .models import Category
 from .models import Task
 
+import time
+
+class TimestampField(serializers.Field):
+	def to_representation(self, value):
+		return time.mktime(value.timetuple())
+
+
+
 class UserSerializer(serializers.ModelSerializer):
+	userId = serializers.IntegerField(source='id')
+
 	class Meta:
 		model = User
-		fields = ('id', 'username')
-
-class ModelSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Category
-		fields = ('id', 'name', 'user')
+		fields = ('userId', 'username')
 
 class TaskSerializer(serializers.ModelSerializer):
+	taskId = serializers.IntegerField(source='id')
+	deadline = TimestampField()
 	class Meta:
 		model = Task
-		fields = ('id', 'name', 'owner', 'viewer', 'deadline', 'last_notify_ts', 'category')
+		fields = ('taskId', 'name', 'owner', 'viewer', 'deadline', 'description', 'category')
 
-class IdSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+	categoryId = serializers.IntegerField(source='id')
+
+	class Meta:
+		model = Category
+		fields = ('categoryId', 'name')
+
+'''
+ID Serializers
+'''
+class TaskIdSerializer(serializers.ModelSerializer):
+	taskId = serializers.IntegerField(source='id')
 	class Meta:
 		model = Task
-		fields = ('id',)
+		fields = ('taskId',)
+
