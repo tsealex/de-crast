f0f8c4d9417f3f959d279e1803473a6221e108f7
tdohm <timdohm@gmail.com>
2017-04-05 15:17:55 -0500

    added automatic sorting by due time in home display

--- De-Crast/www/js/app.js
+++ De-Crast/www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('decrast', ['ionic', 'decrast.controllers', 'decrast.services', 'decrast.server', 'ngOpenFB', 'ngCordova'])
+angular.module('decrast', ['ionic', 'decrast.controllers', 'decrast.services', 'decrast.server', 'ngOpenFB', 'ngCordova', 'ngOrderObjectBy'])
 
 .run(function($ionicPlatform, ngFB) {
   
